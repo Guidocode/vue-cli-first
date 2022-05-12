ec9fba0c1805906d89075e86dd45bae15474410c
Guidocode <guidobuono@icloud.com>
2022-05-12 15:37:08 +0200

    primo vue cli

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,16 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitleComp from './components/TitleComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TitleComp
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/TitleComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="title">“La mia prima app con Vue CLI!”</h1>
+</template>
+
+<script>
+export default {
+  name: 'TitleComp',
+  
+}
+</script>
+
+<style>
+
+</style>
